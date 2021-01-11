487a053c0b98704f70d52a8ce0b6330c98145800
Wmr <wemerson.21outlook@.com>
2021-01-10 22:52:12 -0300

    Projeto iniciado mostrando a mensagem Ola mundo

---
+++ Projeto_De_Teste/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_De_Teste
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Olá Mundo");
+
+			Console.ReadLine();
+		}
+	}
+}
